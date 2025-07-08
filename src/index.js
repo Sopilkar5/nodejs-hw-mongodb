430d6334a462ff0b99e703c4204e91d229ef4ae4
Serhiy Tatomyre <combat.ygar@gmail.com>
2025-07-08 17:58:11 +0300

    nice

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import dotenv from 'dotenv';
-import swaggerUi from 'swagger-ui-express';
-import fs from 'fs';
-import path from 'path';
 
 dotenv.config();
 
 async function startApplication() {
   try {
     await initMongoConnection();
-    const app = setupServer();
-    const swaggerDocument = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'docs/swagger.json')));
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    setupServer();
   } catch (error) {
     console.error('Failed to start application:', error);
     process.exit(1);
